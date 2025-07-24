6ac253774a584d3e3d20f0cf112cbfdd9454da8e
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-07-24 16:49:41 +0530

    Update basePath in next.config.ts to '/listings' and set trailingSlash to false

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/plot-listing'
+  basePath: '/listings',
+  trailingSlash: false,
 };
 
 export default nextConfig;

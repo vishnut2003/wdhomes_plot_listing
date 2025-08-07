a415294daf10eb235c0a9953c5e098a5beb99ded
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-08-07 17:46:08 +0530

    fixed Props issue by change page to index and replaced in imported file

---
+++ src/app/SingleListingPage/index.tsx
@@ -0,0 +1,14 @@
+
+const SingleListingPage = ({}: {
+    location: string,
+    slug: string,
+}) => {
+  return (
+    <div>
+        <p>Location:</p>
+        <p>Slug:</p>
+    </div>
+  )
+}
+
+export default SingleListingPage
